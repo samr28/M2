3f8133a977d767cc96f1706568a602e51dfbe841
farhibahsan <44308788+farhibahsan@users.noreply.github.com>
2019-09-16 23:40:41 -0400

    Implemented calc

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	  String returner = "";
+	  for (int i = input.length(); i >= 0; i--) {
+	  	returner += input.charAt(i);
+	  }
+	  return returner;
 	}
 }
